04ba975c6c82a0bad835e3e7600a2f5815eef9a2
DavidmB <david.m.bedford1@gmail.com>
2019-05-28 22:42:01 -0400

    schema and seeds set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie bacon cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Avacado Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Italian Seitan burger", false);
